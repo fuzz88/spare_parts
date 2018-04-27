638deaf52a3578920a9fc725b50401a56b31ef9a
fuzz88 <ivan@oschepkov.ru>
2018-04-27 11:54:12 +0800

    Add /order route; refactor controllers

--- manager/__init__.py
+++ manager/__init__.py
@@ -5,8 +5,14 @@ from manager.models import SparePartsManager
 
 app = Flask(__name__)
 
+manager = SparePartsManager()
+
 
 @app.route('/')
 def index():
-    manager = SparePartsManager()
-    return render_template('test.j2', parts=manager.parts_grouped)
+    return render_template('index.j2', parts=manager.parts_grouped)
+
+
+@app.route('/order')
+def order():
+    return render_template('order.j2', parts=manager.parts_for_order)
