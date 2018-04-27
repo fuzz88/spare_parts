acf87c729a698c9ea57a45725c6b8d7a7a884055
fuzz88 <ivan@oschepkov.ru>
2018-04-27 09:38:22 +0800

    Rename alt_name variable (now it's name is more precise)

--- manager/models.py
+++ manager/models.py
@@ -26,15 +26,15 @@ class SparePartsManager(object):
         parts_grouped = {}
 
         for part in self._parts:  # Let's iterate through the all parts
-            alt_name = self.get_alternative_group(part)  # Does the part have an alternative group?
-            if alt_name is None:
+            group_name = self.get_alternative_group(part)  # Does the part have an alternative group?
+            if group_name is None:
                 parts_grouped[part] = self._parts[part]  # No, keep data unchanged
             else:
-                if alt_name not in parts_grouped:
-                    parts_grouped[alt_name] = {'count': 0, 'arrive': 0, 'mustbe': []}  # Yes, in this case we should make a group
-                parts_grouped[alt_name]['count'] += self._parts[part]['count']
-                parts_grouped[alt_name]['arrive'] += self._parts[part]['arrive']
-                parts_grouped[alt_name]['mustbe'].append(self._parts[part]['mustbe'])  # There will be list for 'mustbe' values, at first
+                if group_name not in parts_grouped:
+                    parts_grouped[group_name] = {'count': 0, 'arrive': 0, 'mustbe': []}  # Yes, in this case we should make a group
+                parts_grouped[group_name]['count'] += self._parts[part]['count']
+                parts_grouped[group_name]['arrive'] += self._parts[part]['arrive']
+                parts_grouped[group_name]['mustbe'].append(self._parts[part]['mustbe'])  # There will be list for 'mustbe' values, at first
 
                 # TODO: maybe we would like to save the alternatives for the future, like
                 # parts_grouped[group_name]['alt_list'].append(self._parts[part])
@@ -52,7 +52,7 @@ class SparePartsManager(object):
             return None
 
     def get_alternative_group(self, part_name):
-        for alt_name in self._alternatives['alternatives']:
-            if part_name in self._alternatives['alternatives'][alt_name]:
-                return alt_name
+        for group_name in self._alternatives['alternatives']:
+            if part_name in self._alternatives['alternatives'][group_name]:
+                return group_name
         return None
