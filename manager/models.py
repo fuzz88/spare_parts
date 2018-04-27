cdb0021251da9a0366319611f6ad9058f2ec5c47
fuzz88 <ivan@oschepkov.ru>
2018-04-27 11:41:28 +0800

    Add parts_for_order method implementation; refactor class

--- manager/models.py
+++ manager/models.py
@@ -20,21 +20,27 @@ class SparePartsManager(object):
 
     @property
     def parts_grouped(self):
-        if self._parts is None:  # If there is no parts data then...
-            return None  # return None, so it will be handled on the level above.
+        if self._parts is None:
+            # If there is no parts data then...
+            return None  # it will be handled on the level above.
 
         parts_grouped = {}
 
-        for part in self._parts:  # Let's iterate through the all parts
-            group_name = self.get_alternative_group(part)  # Does the part have an alternative group?
+        for part in self._parts:
+            # Iterate through the all parts
+            group_name = self.get_alternative_group(part)
+            # Does the part have an alternatives group?
             if group_name is None:
-                parts_grouped[part] = self._parts[part]  # No, keep data unchanged
+                parts_grouped[part] = self._parts[part]  # No: keep data unchanged
             else:
                 if group_name not in parts_grouped:
-                    parts_grouped[group_name] = {'count': 0, 'arrive': 0, 'mustbe': []}  # Yes, in this case we should make a group
+                    # Yes: in that case we should make a group, if it's not already exists,
+                    parts_grouped[group_name] = {'count': 0, 'arrive': 0, 'mustbe': []}
+                #  and sum up values
                 parts_grouped[group_name]['count'] += self._parts[part]['count']
                 parts_grouped[group_name]['arrive'] += self._parts[part]['arrive']
-                parts_grouped[group_name]['mustbe'].append(self._parts[part]['mustbe'])  # There will be list for 'mustbe' values, at first
+                # There will be list for 'mustbe' values, at first
+                parts_grouped[group_name]['mustbe'].append(self._parts[part]['mustbe'])
 
                 # TODO: maybe we would like to save the alternatives for the future, like
                 # parts_grouped[group_name]['alt_list'].append(self._parts[part])
@@ -43,13 +49,26 @@ class SparePartsManager(object):
             if isinstance(parts_grouped[part]['mustbe'], list):
                 max_value = max(parts_grouped[part]['mustbe'])
                 del parts_grouped[part]['mustbe']
-                parts_grouped[part]['mustbe'] = max_value  # Let's replace 'mustbe' list with max_value of the list
+                # Replace 'mustbe' list with max_value of the list, at second
+                parts_grouped[part]['mustbe'] = max_value
 
         return parts_grouped
 
-    def parts_to_order(self):
+    @property
+    def parts_for_order(self):
         if self._parts is None:
             return None
+
+        order = {}
+
+        parts = self.parts_grouped
+
+        for part in parts:
+            avail_and_awaiting = parts[part]['count'] + parts[part]['arrive']
+            if parts[part]['mustbe'] > avail_and_awaiting:
+                order[part] = {'quantity': parts[part]['mustbe'] - avail_and_awaiting}
+
+        return order
 
     def get_alternative_group(self, part_name):
         for group_name in self._alternatives['alternatives']:
