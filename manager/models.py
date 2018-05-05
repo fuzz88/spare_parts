fe35d00daaf2173cd15bf3f0101c856c0c0e3c0c
fuzz88 <ivan@oschepkov.ru>
2018-05-05 13:14:47 +0800

    Retouch models

--- manager/models.py
+++ manager/models.py
@@ -4,6 +4,7 @@ import requests
 
 
 class SparePart(object):
+    """ Manages single spare part data. """
 
     def __init__(self, name, count=0, arrive=0, mustbe=0):
         self._name = name
@@ -24,6 +25,7 @@ class SparePart(object):
 
     @property
     def order_quantity(self):
+        """ Returns quantity we should order. """
         quantity = self._count + self._arrive
         if self._mustbe > quantity:
             return self._mustbe - quantity
@@ -56,8 +58,8 @@ class SparePart(object):
         self._arrive = value
 
     def __repr__(self):
-        return '{} {} {} {}'.format(self._name, self._count,
-                                    self._arrive, self._mustbe)
+        return '{} c:{} a:{} m:{}'.format(self._name, self._count,
+                                          self._arrive, self._mustbe)
 
 
 class SparePartsManager(object):
@@ -76,6 +78,7 @@ class SparePartsManager(object):
 
     @property
     def parts_grouped(self):
+        """ Returns spare parts` dict grouped by alternatives. """
         parts_grouped = {}
         for part in self._parts_grouped():
             parts_grouped.update(self._dict_from_part(part))
@@ -83,19 +86,19 @@ class SparePartsManager(object):
 
     @property
     def parts_for_order(self):
+        """ Returns spare parts` dict we should order. """
         parts_for_order = {}
         for part in self._parts_for_order():
             parts_for_order.update(self._order_dict_from_part(part))
         return parts_for_order
 
     def _parts_grouped(self):
-        """ TODO """
-
+        """ Returns SparePart's` list grouped by alternatives. """
         grouped = []
 
         for part in self._parts:
-            group_name = self._get_alternative_group(part.name)
-            if group_name is None:
+            group_name = self._get_part_alternative(part.name)
+            if not group_name:
                 grouped.append(part)
             else:
                 group = self._find_in_parts(group_name, grouped)
@@ -105,21 +108,21 @@ class SparePartsManager(object):
                 group.mustbe = part.mustbe
                 group.count += part.count
                 group.arrive += part.arrive
-
         return grouped
 
     def _parts_for_order(self):
-        """ TODO """
+        """ Returns SparePart's` list we should order. """
         parts = self._parts_grouped()
         return [part for part in parts if part.order_quantity != 0]
 
-    def _get_alternative_group(self, part_name):
+    def _get_part_alternative(self, part_name):
         """Returns alternative name of the spare part. """
-
         for group_name, group in self._alternatives['alternatives'].items():
             if part_name in group:
                 return group_name
         return None
+
+    """ Helpers """
 
     @staticmethod
     def _find_in_parts(name, parts):
@@ -150,5 +153,4 @@ class SparePartsManager(object):
             yield SparePart(name,
                             values['count'],
                             values['arrive'],
-                            values['mustbe']
-                            )
+                            values['mustbe'])
